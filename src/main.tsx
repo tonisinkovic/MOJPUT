7bef8de915b5ba58c9bcd3c936207a17b9a9eb1e
unknown <josipsinkovic6@gmail.com>
2026-04-21 20:53:49 +0200

    sahur

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,24 @@ import "./index.css";
 
 registerSW({ immediate: true });
 
+const gaMeasurementId = import.meta.env.VITE_GA_MEASUREMENT_ID;
+
+if (gaMeasurementId) {
+  const gtagScript = document.createElement("script");
+  gtagScript.async = true;
+  gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`;
+  document.head.appendChild(gtagScript);
+
+  const inlineScript = document.createElement("script");
+  inlineScript.text = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${gaMeasurementId}');
+  `;
+  document.head.appendChild(inlineScript);
+}
+
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
     <App />
